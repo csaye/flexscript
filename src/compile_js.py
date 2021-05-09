2fa7041faa7ef8f38c38b3008f757caf52cd61a0
Cooper Saye <coopersaye@gmail.com>
2021-05-09 09:53:27 -0600

    Compile commands.

--- src/compile_js.py
+++ src/compile_js.py
@@ -1,2 +1,21 @@
-def compile_js(program):
-    print('compiling js')
+def compile_js(outpath, commands):
+
+    program = ''
+
+    # for each command
+    for command in commands:
+
+        type = command.type
+        args = command.args
+
+        if type == 'function-call':
+            program += f'{args[0]}({", ".join(args[1:])});\n'
+        elif type == 'var-assign':
+            program += f'{args[0]} = {args[1]};\n'
+
+    # write program to file
+    file = open(outpath, 'w')
+    file.write(program)
+    file.close()
+
+    print(f'compiled successfully into {outpath}')
--- src/compile_py.py
+++ src/compile_py.py
@@ -1,2 +1,21 @@
-def compile_py(program):
-    print('compiling py')
+def compile_py(outpath, commands):
+
+    program = ''
+
+    # for each command
+    for command in commands:
+
+        type = command.type
+        args = command.args
+
+        if type == 'function-call':
+            program += f'{args[0]}({", ".join(args[1:])})\n'
+        elif type == 'var-assign':
+            program += f'{args[0]} = {args[1]}\n'
+
+    # write program to file
+    file = open(outpath, 'w')
+    file.write(program)
+    file.close()
+
+    print(f'compiled successfully into {outpath}')
